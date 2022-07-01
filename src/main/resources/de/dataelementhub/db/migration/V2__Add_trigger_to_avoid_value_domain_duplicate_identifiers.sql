a2948a0d3bb3d3e8fbbb51344fdb86fd19eb9846
Abishaa Vengadeswaran <vengadeswaran@med.uni-frankfurt.de>
2022-07-01 13:41:52 +0200

    #7 - Check unique constraint on scoped identifier table - Change trigger to fire on an update

--- src/main/resources/de/dataelementhub/db/migration/V2__Add_trigger_to_avoid_value_domain_duplicate_identifiers.sql
+++ src/main/resources/de/dataelementhub/db/migration/V2__Add_trigger_to_avoid_value_domain_duplicate_identifiers.sql
@@ -19,7 +19,7 @@ end;
 $$ language plpgsql;
 
 create trigger no_duplicate_valuedomain_identifiers_trigger
-    before insert
+    before insert or update
     on scoped_identifier
     for each row
 execute procedure no_duplicate_valuedomain_identifiers();
